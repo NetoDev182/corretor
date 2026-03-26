8a0cc71cc9139676dda326d820d86d47dcba23a6
Ademir Neto <93241321+NetoDev182@users.noreply.github.com>
2026-03-26 14:51:57 -0400

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-# Entrypoint principal para a Vercel
-# A Vercel detecta automaticamente este arquivo como o ponto de entrada Flask
-from api.index import app
-
-if __name__ == '__main__':
-    app.run(debug=True, port=3000)
